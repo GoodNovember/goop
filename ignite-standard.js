945347adad931483717269417565a5c27f0ad4a0
Victor <Madhatterblues@gmail.com>
2020-09-03 11:58:18 -0400

    Initialize dependabot/npm_and_yarn/serialize-to-js-3.0.1

---
+++ ignite-standard.js
@@ -0,0 +1,34 @@
+const Bundler = require('parcel-bundler')
+const concurrently = require('concurrently')
+const path = require('path')
+
+const { getPortKey } = require('./utilities/getPortKey.js')
+
+const { devPort } = require('./common/settings.js')
+
+const entryFile = path.join(__dirname, `./src/index.html`)
+
+const options = {
+  port: devPort,
+  target: 'electron',
+  outDir: './build',
+  useMaps: true,
+  watch: true,
+  // detailedReport: true,
+  autoInstall: false
+}
+
+const bundler = new Bundler(entryFile, options)
+bundler.serve(devPort).then(server => {
+  const portKey = getPortKey()
+  const portRegex = /\d+$/g
+  const portString = portRegex.exec(server._connectionKey)[0]
+  if (portString.length > 0) {
+    concurrently([`${portKey}=${portString} && electron . --remote-debugging-port=9223`]).then(() => {
+      server.close()
+      process.exit(0)
+    }).catch(error => {
+      console.error(error)
+    })
+  }
+})
---
+++ src/client.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import { render } from 'react-dom'
+import { App } from './App/components/App.js'
+
+console.error(`Hello I'm a problem!`)
+
+render(<App />, document.getElementById('root'))
